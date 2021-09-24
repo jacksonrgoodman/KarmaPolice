1e027be4eb615a71019c06494afdb456fe9457d9
Jackson <jacksonrgoodman@gmail.com>
2021-09-24 00:36:38 -0500

    added portraits

--- status.py
+++ status.py
@@ -1,30 +1,18 @@
 import checks
+import faces
+
+
+def bottomBar():
+    print("╭――――――――――――――――――――――――――――――――――――――――――――╮")
+    print("│                                            │")
+    print("╰――――――――――――――――――――――――――――――――――――――――――――╯")
 
 
 def image(actor):
     if actor.GetGender() == 1:
         print()
-        print("                     dS$$S$S$S$S$S$S$$Sb                     ")
-        print("                    :$$SSS$S$S$S$S$S$S$$;                    ")
-        print("                    SSPS^S$^$S$S$^'S^S$SS                    ")
-        print("                    $$     ^$D$S$^'D$S$$$                    ")
-        print("                   _SS       `S'    `S'SS_                   ")
-        print("                  :-.|  _           _  |.-;                  ")
-        print("                  :\(;     _.' '.  __  |)/;                  ")
-        print("                   \`| \(o)\    /(o)/  |'/                   ")
-        print("                    *:     -'  │`-     ;*                    ")
-        print("                      ;.       │      :                      ")
-        print("                      : `    ._.    ' ;                      ")
-        print("                    .sSb   ._____.'  dSs.                    ")
-        print("                 _.d8dSSb.   ._.   .SSbT8b._                 ")
-        print("             _.oOPd88SSSS T.     .P SSSS888OOo.              ")
-        print("         _.mMMOOPd888SSSSb TSqqqSP dSSSS88OMOOOMm._          ")
-        print("      .oOMMMOMOOM8O8OSSSSSb TSSSP dSSSSS8OOMMOOMMOOOo._      ")
-        print("   dOOOMMMOMMOOOMOOOO      `^SSSS^`   :OOO8MMMOOMMOOMMOMMb   ")
-        print("╭―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╮")
-        print("│                                                           │")
-        print("╰―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╯")
-
+        faces.oldman1()
+        bottomBar()
     if actor.GetGender() == 2:
         print()
         print("            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀           ")
